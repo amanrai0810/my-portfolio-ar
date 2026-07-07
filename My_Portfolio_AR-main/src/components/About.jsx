694a64cd3ea5157555e0b19c50ba33442873f7aa
amanrai6204 <amanrai6204@gmail.com>
2026-07-07 20:40:06 +0530

    Portfolio Website

---
+++ My_Portfolio_AR-main/src/components/About.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import '../App.css';
+import { services } from '../constants';
+import ButtonLink from './ButtonLink';
+import Footer from './Footer';
+
+const ServiceCard = ({ service }) => (
+  <div className='sm:w-[250px] w-full'>
+    <div
+      className='w-full green-pink-gradient p-[1px] rounded-[20px]'>
+      <div
+        className='rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        style={{ background: '#151030' }}>
+        <img
+          src={service.icon}
+          alt='some_icon'
+          className='object-contain w-16 h-16'
+        />
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {service.title}
+        </h3>
+      </div>
+    </div>
+  </div>
+);
+
+const About = () => {
+  return (
+    <div>
+
+      <div className='w-full h-full py-12 mt-8 overflow-x-hidden text-white bg-black sm:flex sm:justify-around about' id='about'>
+        <div className='flex flex-col justify-around'>
+          <div className='px-2 sm:px-16'>
+            <h2 className='mt-2 text-4xl font-extrabold sm:text-5xl'>Introduction</h2>
+            <p className='mt-3 mb-6 text-[17px] max-w-3xl leading-[30px]'>👨‍💻 Hi, I'm Aman Rai, a 2026 graduate🎓, with a passion for designing dynamic user interfaces and developing scalable Java applications. As an experienced <a className='text-green-300 duration-300 hover:text-green-500' href='https://www.linkedin.com/in/amanrai21/' target='_blank'>Frontend Developer and Java Developer</a>, I specialize in System Design🚀.
+              <br />
+              My technical skills include:<br/>
+              Languages: Java, SQL, JavaScript<br/>
+              Skills: ReactJS, TailwindCSS, HTML/CSS, Problem Solving<br/>
+
+              ✍️ Beyond coding, I love to engage in problem-solving on LeetCode, where I delve into the intricacies of data structures and algorithms. On a different note, I'm also passionate about traveling✈️. Exploring new places, cultures, and experiences fuels my creativity and gives me a fresh perspective on life.</p>
+
+
+
+
+            <ButtonLink
+              url='https://drive.google.com/file/d/1jygvYUU3pPFfEYfbCSLH_ZHn25M7OwG9/view'
+              text='View Resume →'
+              padding={`p-3`}
+            />
+
+          </div>
+          <div className='flex flex-wrap justify-center mt-20 gap-7'>
+            {services.map((service) => (
+              <ServiceCard service={service} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default About;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,80 @@
+
+import React from "react";
+//import hotelMoon from '../assets/hotel_moon.png'
+import ticTacImage from '../assets/tic_tac_toe.png'
+//import stockImage from '../assets/Stock.png'; 
+import interviewPrepImage from '../assets/Interview.png';
+import Footer from './Footer'
+
+const ProjectCard = ({image, title, description, git, technologies }) => {
+    return (
+        <div className="max-w-sm bg-gray-900 border rounded-lg shadow sm:max-w-sm md:max-w-sm border-neutral-100 dark:bg-gray-800 dark:border-gray-700">
+            {title==='Tic Tac Toe With Ai' && <a href="#">
+                <img className="object-cover w-full h-auto rounded-t-lg " src={image} alt="" />
+            </a>}
+            {title==='INTERVIEW-PREP' && <a href="#">
+                <img className="object-cover w-full h-auto rounded-t-lg " src={image} alt="" />
+            </a>}
+            <div className="p-4 sm:p-6">
+                <a href="#">
+                    <h5 className="text-2xl font-bold tracking-tight text-transparent text-white sm:text-xl md:text-2xl lg:text-3xl bg-clip-text bg-gradient-to-r from-yellow-200 to-pink-500">{title}</h5>
+                </a>
+                <p className="text-sm font-normal text-gray-300 sm:text-base md:text-lg dark:text-gray-400">{description}</p>
+            </div>
+            <div className='flex justify-between m-2 sm:m-4 lg:m-6'>
+                <div className='flex flex-wrap gap-2 pl-2'>
+                    {technologies.map((tag, index) => (
+                        <p
+                            key={`${index}-${tag}`}
+                            className='text-[14px] text-blue-500'
+                        >
+                            #{tag}
+                        </p>
+                    ))}
+                </div>
+                <a href={git} className="p-1 text-red-300 duration-300 border border-gray-200 rounded-lg shadow sm:p-2 lg:p-3 hover:text-green-500">GitHub</a>
+            </div>
+        </div>
+    );
+};
+  
+const Projects = () => {
+    return (
+        <div className="bg-black">
+            <div className="flex flex-wrap items-center justify-center p-12 m-12 gap-7">
+                {project.map((item, index) => (
+                    <ProjectCard
+                        key={index}
+                        image={item.image}
+                        title={item.title}
+                        description={item.description}
+                        links={item.links}
+                        git={item.git}
+                        technologies={item.technologies}
+                    />
+                ))}
+            </div>
+            <Footer/>
+        </div>
+    );
+}
+
+
+export const project = [
+    {
+        title:'Tic Tac Toe With Ai',
+        description: 'Developed an interactive Tic Tac Toe game with AI integration using HTML, CSS, and JavaScript, featuring an intelligent opponent powered by the Minimax algorithm for unbeatable gameplay. Designed a visually appealing and responsive user interface with smooth animations and engaging sound effects to enhance the player experience. The project includes both single-player (against AI) and two-player modes, real-time game state tracking, and dynamic win detection, delivering a fun and challenging experience across devices.',
+        image: ticTacImage,
+        git:'https://github.com/amanrai0810/Tic-tac-toe-game-with-AI',
+        technologies:['HTML' , 'CSS', 'JavaScript']
+    },
+    {
+        title:'INTERVIEW-PREP',
+        description:'INTERVIEW-PREP is a dynamic web-based platform designed to help students and job seekers prepare effectively for technical and HR interviews. The platform offers curated resources, including frequently asked coding problems, aptitude questions, behavioral interview tips, and commonly asked HR questions.',
+        image: interviewPrepImage,
+        git:'https://github.com/amanrai0810/Interview-Prep',
+        technologies:[ 'HTML', 'CSS', 'JavaScript', 'React']
+    }
+]
+
+export default Projects
